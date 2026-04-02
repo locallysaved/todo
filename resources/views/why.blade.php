f0fe9693d40aa65653f566f47b21798736415432
locallysaved <vanilla2020xd@gmail.com>
2026-04-02 11:21:20 +0300

    type shift

--- resources/views/why.blade.php
+++ resources/views/why.blade.php
@@ -7,7 +7,15 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>kapec mest 500 eur menesi uz hustlers university?</h1>
-    <p>HUSTLERS UNIVERSITY GOATED THATS WHY</p>
+    <p>
+        Mūsdienu straujajā dzīves ritmā efektīva laika plānošana un uzdevumu pārvaldība ir kļuvusi par būtisku ikdienas sastāvdaļu. Darāmo darbu saraksta vietne piedāvā strukturētu pieeju, kas palīdz lietotājiem organizēt savus pienākumus un sasniegt mērķus.
+
+        Kāpēc darāmo darbu saraksts ir nepieciešams ikvienam?
+        1. Uzdevumu organizēšana un prioritizēšana: Saraksts ļauj skaidri saskatīt visus veicamos uzdevumus, palīdzot noteikt to svarīgumu un steidzamību. Tas novērš haosu un palīdz koncentrēties uz būtiskāko.
+        2. Stresa samazināšana: Pierakstot uzdevumus, mēs atbrīvojam prātu no nepieciešamības tos atcerēties, tādējādi mazinot trauksmi un pārslodzi.
+        3. Produktivitātes paaugstināšana: Sistemātiska uzdevumu izpilde un to atzīmēšana kā paveikti sniedz gandarījumu un motivē turpināt darbu, palielinot efektivitāti.
+        4. Laika pārvaldība: Plānojot dienu ar darāmo darbu sarakstu, ir vieglāk sadalīt laiku starp dažādiem uzdevumiem, nodrošinot, ka nekas netiek aizmirsts un visi termiņi tiek ievēroti.
+        5. Līdzsvars starp darbu un privāto dzīvi: Saraksts palīdz nodalīt profesionālos pienākumus no personīgajiem, veicinot veselīgu darba un dzīves balansu.
+    </p>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -6,22 +6,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/results', function () {
-    return view('results');
-});
-
-Route::get('/courses', function () {
-    return view('courses');
-});
-
-Route::get('/price', function () {
-    return view('price');
-});
-
-Route::get('/signup', function () {
-    return view('signup');
-});
-
 Route::get('/why', function () {
     return view('why');
 });
